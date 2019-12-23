e527998ed0965a14cfc216e80b94cd678a2221b6
Charley McGowan <glitchwizard@gmail.com>
2019-12-22 23:54:43 -0800

    SignUp form complete, with some validation in place too

--- react-firebase-authentication/src/components/SignUp/index.tsx
+++ react-firebase-authentication/src/components/SignUp/index.tsx
@@ -1,11 +1,100 @@
 import React from 'react'
 
-const SignUp = () => {
-    return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
-    )
+import * as ROUTES from '../../constants/routes';
+
+const SignUpPage = () => (
+    <div>
+        <h1>SignUp</h1>
+        <SignUpForm />
+    </div>
+)
+
+interface Props {
+    
+}
+interface State {
+    username: string,
+    email: string,
+    passwordOne: string,
+    passwordTwo: string,
+    error: any
 }
 
-export default SignUp;
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null
+};
+
+class SignUpForm extends React.Component<Props, State> {
+    constructor(props: any) {
+        super(props)
+        this.state = { ...INITIAL_STATE}
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onSubmit = (event: any) => {
+
+    }
+
+    onChange = (event: any) => { 
+        //@ts-ignore
+        this.setState({ [event.target.name]: event.target.value });
+    };
+    
+    render() {
+        const {
+            username,
+            email,
+            passwordOne,
+            passwordTwo,
+            error
+        } = this.state;
+
+        const isInvalid = 
+        passwordOne !== passwordTwo ||
+        passwordOne === '' ||
+        email.length < 4 ||
+        username.length < 4; 
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input 
+                    name="username"
+                    value={username}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Full Name"
+                />
+                <input
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input
+                    name="passwordOne"
+                    value={passwordOne}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                />
+                <input
+                    name="passwordTwo"
+                    value={passwordTwo}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <button disabled={isInvalid} type="submit">Sign Up</button>
+
+                {error && <p>{error.message}</p>}
+            </form>
+        );
+    }
+}
+export default SignUpPage;
